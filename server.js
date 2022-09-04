c72f49f7c3a388bdf58860de9171ea85f0ab59fa
shubhpro <shubhpro@shubhams-MacBook-Pro.local>
2022-09-04 19:54:29 +0530

    react static folder

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express=require("express");
 const app=express();
 
+app.use(express.static('dist'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  });
+
+
+
+/*
 app.get("/",function(req,res){
     res.json({
         message:"Api Success"
     })
 })
+*/
+
+// how to send the static file
 
 app.listen(8080,function(){
     console.log(`Server Running `);
